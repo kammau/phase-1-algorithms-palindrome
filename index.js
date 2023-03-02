9bfcfe786c3397df251e9d04d58937c45b590bf5
Kamryn Smith <programersign@gmail.com>
2023-03-02 11:16:57 -0800

    assignment complete

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  if (word === word.split("").reverse().join("")) {
+    return true
+  } else {
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+  if string = stringBackwards:
+    return true
+  else:
+    return false
 */
 
 /*
   Add written explanation of your solution here
+  If inputed word is equal to itself backwards the function returns true if it is not equal to itself backwards
+  the function returns false.
 */
 
 // You can run `node index.js` to view these console logs
